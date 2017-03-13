8f85616411faceb7b2228f0857600861244c68ab
Minsik Lee <minsikl@gmail.com>
2017-03-13 17:43:02 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo "**************** Start *******"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 10 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 20 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 30 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 40 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 50 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 60 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 70 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 80 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** 90 mins *****"
+/bin/sleep 300
+/bin/sleep 300
+echo "**************** End ******"
