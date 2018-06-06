0fea60fa36510e75e02c4376d31d8c5cfe5bd9a2
Minsik Lee <minsikl@gmail.com>
2018-06-06 09:54:51 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,3 @@
-Get-Process | Out-File -filepath C:\process.txt
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+wget https://github.com/minsikl/cheatsheet/blob/master/putty.exe?raw=true -outfile "C:\putty.exe"
+C:\putty.exe
