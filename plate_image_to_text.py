987c0f075239434eba62f9b374956a15c1179e2e
Minsik Lee <minsikl@gmail.com>
2018-10-17 02:26:41 -0400

    Update plate_image_to_text.py

--- plate_image_to_text.py
+++ plate_image_to_text.py
@@ -3,31 +3,56 @@ import time
 import logging
 import subprocess
 import json
+from threading import Thread
 
 source_dir = '/incoming_images'
 license_plate_dir = '/license_plates'
 
+def handle_single_message(incoming_image):
+    p = subprocess.Popen("/usr/bin/alpr -c us -n 1 -j " + source_dir + "/" + incoming_image, stdout=subprocess.PIPE, shell=True)
+    (alpr_output_bytes, err) = p.communicate()
+    p_status = p.wait()
+    alpr_output_str = alpr_output_bytes.decode("utf-8")
+    print(alpr_output_str)
+    alpr_output = json.loads(alpr_output_str)
+    if len(alpr_output['results']) > 0:
+        license_plate_number = alpr_output['results'][0]["plate"]
+        p = subprocess.Popen("/bin/cp " + source_dir + "/" + incoming_image + " " + license_plate_dir + "/" + license_plate_number + "_" + incoming_image, stdout=subprocess.PIPE, shell=True)
+        p_status = p.wait()
+    os.unlink(source_dir + "/" + incoming_image)
 
 def handle_message():
     while True:
         incoming_images = os.listdir(source_dir)
-        if len(incoming_images) == 0:
+        incoming_images_len = len(incoming_images)
+        if incoming_images_len > 10:
+            incoming_images_len = 10
+        if incoming_images_len == 0:
             print("No available image. Sleep 3 secs...")
             time.sleep(3)
         else:
-            print(incoming_images[0])
-            p = subprocess.Popen("/usr/bin/alpr -c us -n 1 -j " + source_dir +
-                                 "/" + incoming_images[0], stdout=subprocess.PIPE, shell=True)
-            (alpr_output_bytes, err) = p.communicate()
-            p_status = p.wait()
-            alpr_output_str = alpr_output_bytes.decode("utf-8")
-            print(alpr_output_str)
-            alpr_output = json.loads(alpr_output_str)
-            if len(alpr_output['results']) > 0:
-                license_plate_number = alpr_output['results'][0]["plate"]
-                p = subprocess.Popen("/bin/cp " + source_dir + "/" + incoming_images[0] + " " + license_plate_dir + "/" + license_plate_number + "_" + incoming_images[0], stdout=subprocess.PIPE, shell=True)
-                p_status = p.wait()
-            os.unlink(source_dir + "/" + incoming_images[0])
+            threads = []
+            for i in range(incoming_images_len):
+                t = Thread(target=handle_single_message, args=(incoming_images[i],))
+                threads.append(t)
+            for t in threads:
+                t.start()
+                print("Start a thread.")
+            for t in threads:
+                t.join()
+#            print(incoming_images[0])
+#            p = subprocess.Popen("/usr/bin/alpr -c us -n 1 -j " + source_dir +
+#                                 "/" + incoming_images[0], stdout=subprocess.PIPE, shell=True)
+#            (alpr_output_bytes, err) = p.communicate()
+#            p_status = p.wait()
+#            alpr_output_str = alpr_output_bytes.decode("utf-8")
+#            print(alpr_output_str)
+#            alpr_output = json.loads(alpr_output_str)
+#            if len(alpr_output['results']) > 0:
+#                license_plate_number = alpr_output['results'][0]["plate"]
+#                p = subprocess.Popen("/bin/cp " + source_dir + "/" + incoming_images[0] + " " + license_plate_dir + "/" + license_plate_number + "_" + incoming_images[0], stdout=subprocess.PIPE, shell=True)
+#                p_status = p.wait()
+#            os.unlink(source_dir + "/" + incoming_images[0])
 
 
 def create_daemon():
