c5dafa4c13c551d3d259a42c2a927f387571e83d
Minsik Lee <minsikl@gmail.com>
2019-08-09 00:13:40 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,9 +1,17 @@
-variable cmd {
-  description = "test"
+variable cmd1 {
+  description = "test1"
+  default   = ""
+}
+variable cmd2 {
+  description = "test2"
+  default   = ""
+}
+variable cmd3 {
+  description = "test3"
   default   = ""
 }
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "${var.cmd}"
+    command = "${var.cmd1} ${var.cmd2} ${var.cmd3}"
   }
 }
